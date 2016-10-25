cbb844e396748304e5348b9b427ad13596c63c3d
MrGeejerome <jokakillaz640@gmail.com>
2016-10-25 08:38:32 -0700

    Add files via upload

---
+++ Asc.java
@@ -0,0 +1,18 @@
+// file: Asc.java
+// javac Asc.java
+// java Asc
+class Asc {
+	public static void main (String a[]){
+		//variables
+		int i;// integer i
+		char c;// character c
+		//for loop
+		for (i = 0; i < 100; i++){
+			c = (int)i;
+			System.out.print(i);
+			System.out.print(" ");
+			System.out.print(c);
+			System.out.println();
+		}
+	}
+}
